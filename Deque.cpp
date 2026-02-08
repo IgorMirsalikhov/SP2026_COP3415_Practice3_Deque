16cb6465aefd38573313740071e8d317e143c06f
Igor Mirsalikhov <imirsalikhov@floridapoly.edu>
2026-02-08 14:46:41 -0500

    Add files via upload

---
+++ Deque.cpp
@@ -0,0 +1,65 @@
+#include "Deque.hpp"
+#include "Node.hpp"
+
+#include <iostream>
+#include <stdexcept>
+
+template <typename T>
+Deque<T>::Deque() {
+    
+}
+
+template <typename T>
+Deque<T>::~Deque() {
+    
+}
+
+template <typename T>
+void Deque<T>::push_front(const T& val) {
+    
+}
+
+template <typename T>
+void Deque<T>::push_back(const T& val) {
+    
+}
+
+template <typename T>
+void Deque<T>::print() const {
+    
+}
+
+template <typename T>
+T Deque<T>::pop_front() {
+    
+}
+
+template <typename T>
+T Deque<T>::pop_back() {
+    
+}
+
+template <typename T>
+bool Deque<T>::empty() const {
+    
+}
+
+template <typename T>
+const T& Deque<T>::front() const {
+    
+}
+
+template <typename T>
+const T& Deque<T>::back() const {
+    
+}
+
+template <typename T>
+int Deque<T>::size() const {
+  
+}
+
+template <typename T>
+void Deque<T>::clear() {
+    
+}
